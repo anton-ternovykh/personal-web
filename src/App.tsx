f79e0e65c294d930ac3b455f779cc96d2179e1b8
Anton T <Teranton@gmail.com>
2025-11-20 10:44:24 -0800

    * ReactJS added. * Public files moved to dist folder

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to Anton Ternovykh's Portfolio</h1>
+      <p>This is a placeholder for the portfolio content.</p>
+    </>
+  );
+}
+
+export default App;
